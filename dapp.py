3ab0927429c41e17892f0fc9a9204704ef830a0a
msimon42 <msimon@uab.edu>
2020-02-15 23:49:43 -0700

    Add query to get all dapps belonging to certain category

--- dapp.py
+++ dapp.py
@@ -1,3 +1,6 @@
+
+import records
+
 class Dapp:
     def __init__():
         self.conn = records.database('postgres://vummccgjanibtc:e7f4258876e39efe1670951cc47107e77d081a07306bc101ee38facbc6352cb7@ec2-184-72-236-57.compute-1.amazonaws.com:5432/dcj1957cfej19p')
@@ -6,4 +9,11 @@ class Dapp:
         result = self.conn.query(f"SELECT organizations.name FROM contracts INNER JOIN organizations ON organizations.id = contracts.organization_id WHERE contracts.address = {address}")
         return result[0]['name']
 
-        
+
+    def dapps_by_category(category):
+        result = self.conn.query(f"SELECT name FROM organizations WHERE category = {category}")
+        names = []
+        for result in results:
+            names.append(result['name'])
+
+        return names
