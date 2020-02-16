1d6583492985241bcf6b66070ab3369bdb9d6555
msimon42 <msimon@uab.edu>
2020-02-16 05:54:37 -0700

    Complete route for dashboard

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask_restful import Resource, Api
 from flask_cors import CORS
 from etherscan_service import EtherscanService
 from address_serializer import AddressSerializer
+from dashboard_serializer import DashboardSerializer
+from dapp import Dapp
 
 
 app = Flask(__name__)
@@ -19,9 +21,12 @@ def addresses(contract):
 def analyze():
     e = EtherscanService()
     data = request.json
+    dapp = Dapp.find_info_by_address(data['dapp_address'])
     users = Dapp.users(data['dapp_address'])
     oois =  Dapp.ooi_info(data['dapp_address'], data['category'])
+    external_oois = Dapp.external_ooi_info(data['dapp_address'], data['category'])
 
+    return DashboardSerializer.render_json(dapp, users, oois, external_oois)
 
 @app.route("/<contracts>/common-addresses", methods=['GET'])
 def in_common(contracts):
