a02063e72b8f18cabcff991fb44359846a0a2c62
msimon42 <msimon@uab.edu>
2020-02-16 03:35:20 -0700

    Add methods to dapp

--- dapp.py
+++ dapp.py
@@ -1,13 +1,13 @@
 import records
-import os
+from etherscan_service import EtherscanService
 
 class Dapp:
     def __init__():
-        self.conn = records.database(os.environ['DATABASE_URL'])
+        self.conn = records.database(Secrets.db_link)
 
-    def find_name_by_address(address):
-        result = self.conn.query(f"SELECT organizations.name FROM contracts INNER JOIN organizations ON organizations.id = contracts.organization_id WHERE contracts.address = {address}")
-        return result[0]['name']
+    def find_info_by_address(address):
+        result = self.conn.query(f"SELECT organizations.name, organizations.category FROM contracts INNER JOIN organizations ON organizations.id = contracts.organization_id WHERE contracts.address = {address}")
+        return result[0]
 
 
     def dapps_by_category(category):
@@ -17,3 +17,28 @@ class Dapp:
             names.append(result['name'])
 
         return names
+
+    def ooi_info(contract, oois):
+        result = {}
+        e = EtherscanService()
+        for ooi in oois:
+            info = self.find_info_by_address(ooi)
+            result.append({
+            'name': info['name'],
+            'category': info['category'],
+            'shared_users': e.unique_users(contract, ooi)
+            })
+
+        return result
+
+    def users(contract, oois):
+        e = EtherscanService()
+        count = len(e.addresses_by_contract(contract))
+        shared = len(e.addresses_in_common(contract, oois))
+        unique = len(e.unique_users(contract, oois))
+
+        return {
+            'count': count,
+            'total_shared': shared,
+            'total_unique': unique
+        }
