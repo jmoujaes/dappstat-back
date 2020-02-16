6b4c486f03768bbc7b3050221f3f3fdbd8d06bd3
msimon42 <msimon@uab.edu>
2020-02-15 20:20:25 -0700

    Add methods for count of addresses and addresses in common

--- etherscan_service.py
+++ etherscan_service.py
@@ -15,7 +15,23 @@ class EtherscanService:
             if item['from'] not in addresses:
                 addresses.append(item['from'])
 
-        return str(len(addresses))
+        return addresses
+
+    def count_of_addresses(self, contract):
+        return str(len(self.addresses_by_contract(contract)))
+
+
+    # def balance_by_address(self, addresses):
+    #     batches = chunk(addresses, 20)
+    #
+    #     for batch in batches:
+    #
+
+    def addresses_in_common(self, contracts):
+        set_1 = set(self.addresses_by_contract(contracts[0]))
+        set_2 = set(self.addresses_by_contract(contracts[1]))
+        return list(set_1.intersection(set_2))
+
 
     def connection(self, params):
         r = requests.get(f"{self.uri}?{params}")
