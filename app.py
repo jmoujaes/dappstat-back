6a72aacfba017c0474832fb3cd2f0b2be5dcb388
msimon42 <msimon@uab.edu>
2020-02-15 22:38:46 -0700

    Move jsonify to address serializer

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask_restful import Resource, Api
 from flask_cors import CORS
 from etherscan_service import EtherscanService
+from address_serializer import AddressSerializer
 import requests
 
 app = Flask(__name__)
@@ -12,14 +13,16 @@ CORS(app)
 def addresses(contract):
     e = EtherscanService()
     addresses = e.addresses_by_contract(contract)
-    return jsonify({'count': len(addresses), 'addresses': addresses})
+    return AddressSerializer.render_json(addresses)
 
 @app.route("/<contracts>/common-addresses", methods=['GET'])
 def in_common(contracts):
     li_contracts = list(contracts.split(','))
     e = EtherscanService()
     addresses_in_common = e.addresses_in_common(li_contracts)
-    return jsonify({'shared_users': {'count': len(addresses_in_common), 'addresses': addresses_in_common}})
+    return AddressSerializer.render_json(addresses_in_common)
+
+
 
 if __name__ == '__main__':
    app.run(debug = True)
